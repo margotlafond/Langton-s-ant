3f2233dcc4583435158577567b95585c956b41f6
Margot Lafond <margot.lafond@etu.minesparis.psl.eu>
2025-02-09 18:46:52 +0100

    end of class tile

---
+++ tile.py
@@ -0,0 +1,31 @@
+# Third party
+import pygame
+
+class Tile:
+    """A square tile in the game. Includes a color."""
+    
+    def __init__(self, x: int, y: int, color: pygame.Color) -> None:
+        """Object initialization."""
+        self._x = x # Column index
+        self._y = y # Line index
+        self._color = color
+
+    @property
+    def x(self) -> int:
+        """The x coordinate (i.e.: column index) of the tile."""
+        return self._x
+    
+    @property
+    def y(self) -> int:
+        """The y coordinate (i.e.: line index) of the tile."""
+        return self._y
+    
+    @property
+    def color(self) -> pygame.Color:
+        """The color of the tile."""
+        return self._color
+    
+    @color.setter
+    def color(self, color: pygame.Color) -> None:
+        """Change the color of the tile."""
+        self._color = color
